900dd9ccc289e0454f16cbf642b2b09e3e1a7688
laaledesiempre <128645176+laaledesiempre@users.noreply.github.com>
2023-09-11 13:41:58 -0300

    basic server for testing

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,40 @@
-console.log("Hello via Bun!");
+// console.log("Hello via Bun!"); --> default message on init
+
+import {Database} from 'bun:sqlite'
+
+const db = new Database(":memory:");
+
+db.query(`create table testing (
+    id interger primary key not null,
+    name text not null);`
+)
+
+serve= bun.serve({
+    port: 3000,
+    fetch(req) {
+
+        const url = new URL(req.url)
+
+// GET
+        if (url.method == "GET") {
+            if (url.pathname == "/api/perros") {
+                const database = db.query(`select * from testing`)
+                return new Response(JSON.parse(database))
+            }
+        }
+// POST 
+        else if (url.method == "POST") {
+            if (url.pathname == "/api/perros") {
+                const {name} = req.body
+                db.query(`insert into testing (name) values (?)`,[name])
+                return new Response(`Inserted ${name} on table`)
+            }
+        }
+// 404
+        else {
+            return new Response(`404 ${url.pathname} not found on server`, {status: 404})
+        }
+
+    }
+}
+)
